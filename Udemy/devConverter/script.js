de3d8b7f5d1cb2267283f10b43ad3068b12e14d6
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-03-19 18:45:53 -0300

    att

--- Udemy/devConverter/script.js
+++ Udemy/devConverter/script.js
@@ -1,5 +1,12 @@
-const form = document.getElementById('form');
-const inputValue = document.getElementById('valor-real');
-const selectCurrency = document.getElementById('currency');
-const resultado = document.getElementById('resultado');
+var form = document.getElementById('form');
+    form.addEventListener('submit', converter) //adicionar um tipo de evento ao clicar em converter
+    
+    var inputValue = document.getElementById('valor-real');
+    var selectCurrency = document.getElementById('currency');
+    var resultado = document.getElementById('resultado');
 
+function converter(e){
+
+    e.preventDefault() //para nao atualizar a pagina automaticamente
+    alert('oi')
+}

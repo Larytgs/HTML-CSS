71c511f6193cfcbb12343d647a5c33bcc6af8616
Larytgs <149098968+Larytgs@users.noreply.github.com>
2024-03-19 17:48:12 -0300

    att

---
+++ Udemy/devConverter/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const inputValue = document.getElementById('valor-real');
+const selectCurrency = document.getElementById('currency');
+const resultado = document.getElementById('resultado');
+
